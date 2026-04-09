a1f8650bff4b578c24b50020c7458a87c20688b3
Evelyn Huang <evelynhuang1116@gmail.com>
2026-04-09 10:26:08 -0400

    Add project files.

---
+++ evel_quiz4_dotnet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace evel_quiz4_dotnet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
